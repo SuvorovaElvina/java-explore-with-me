0f4f618f5bda06da1b780e9cad2dcdb023b9491d
Elvina <elvina.smirnova.205@mail.ru>
2023-07-18 01:43:16 +0300

    style: убрала не используемые импорты

--- server-ewm/src/main/java/ru/practicum/repository/EventRepository.java
+++ server-ewm/src/main/java/ru/practicum/repository/EventRepository.java
@@ -3,13 +3,9 @@ package ru.practicum.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import ru.practicum.enums.State;
-import ru.practicum.model.Category;
 import ru.practicum.model.Event;
-import ru.practicum.model.User;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
