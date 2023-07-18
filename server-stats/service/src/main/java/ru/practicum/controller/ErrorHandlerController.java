01c63d7b94a80e6406a117dd3f8502a17e89b8b0
Elvina <elvina.smirnova.205@mail.ru>
2023-07-19 01:50:11 +0300

    feat: подкорректировала контроллер

--- server-stats/service/src/main/java/ru/practicum/controller/ErrorHandlerController.java
+++ server-stats/service/src/main/java/ru/practicum/controller/ErrorHandlerController.java
@@ -2,6 +2,7 @@ package ru.practicum.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -17,6 +18,18 @@ public class ErrorHandlerController {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ApiError handleValidatedException(final ValidationException e) {
+        log.debug("Получен статус 400 Bad request {}", e.getMessage(), e);
+        return ApiError.builder()
+                .timestamp(LocalDateTime.now())
+                .errors(List.of(e.getStackTrace()))
+                .message(e.getLocalizedMessage())
+                .reason(e.getMessage())
+                .status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleValidatedException(final MethodArgumentNotValidException e) {
         log.debug("Получен статус 400 Bad request {}", e.getMessage(), e);
         return ApiError.builder()
                 .timestamp(LocalDateTime.now())
